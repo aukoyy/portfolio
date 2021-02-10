5b1583f260f1615d3ed1e266c3b039bc6692dbbb
aukoyy <22494963+aukoyy@users.noreply.github.com>
2021-02-10 13:35:45 +0100

    change buildHookId

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -8,13 +8,13 @@ export default {
           {
             title: 'Sanity Studio',
             apiId: 'add2c845-e3aa-4a2e-ad48-f96eca46daf4',
-            buildHookId: 'https://api.netlify.com/build_hooks/6023c5b739bdd518c68a00b9',
+            buildHookId: '6023c5b739bdd518c68a00b9',
             name: 'aukoyy-studio',
           },
           {
             title: 'Website',
             apiId: '28e466ca-7351-461d-bf7e-95c5b26ede02',
-            buildHookId: 'https://api.netlify.com/build_hooks/6023cf77c77ef421c2fece34',
+            buildHookId: '6023cf77c77ef421c2fece34',
             name: 'aukoyy'
           }
         ]
