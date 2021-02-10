1551ef75d1621b63078cc1e130c78c0629e18963
aukoyy <22494963+aukoyy@users.noreply.github.com>
2021-02-10 13:18:29 +0100

    change name

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -15,7 +15,7 @@ export default {
             title: 'Website',
             apiId: '28e466ca-7351-461d-bf7e-95c5b26ede02',
             buildHookId: 'https://api.netlify.com/build_hooks/6023c09206955f0fda05eb35',
-            name: 'aukoyy-web'
+            name: 'aukoyy'
           }
         ]
       }
