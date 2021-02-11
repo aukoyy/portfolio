bf7b9339b0a9a347cedfec86dfbb3172f78297fa
aukoyy <22494963+aukoyy@users.noreply.github.com>
2021-02-11 12:20:42 +0100

    add blog model

--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -1,21 +1,36 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
-import blogPost from './blogPost';
-import sponsor from './sponsor';
+// Document schemas
+import post from './documents/post';
+import category from './documents/category';
+import author from './documents/author';
+
+// Object types
+import authorReference from './objects/authorReference';
+import mainImage from './objects/mainImage';
+import bioPortableText from './objects/bioPortableText';
+import excerptPortableText from './objects/excerptPortableText';
+import bodyPortableText from './objects/bodyPortableText';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    /* Your types here! */
-    blogPost,
-    sponsor
-  ]),
-})
+	// We name our schema
+	name: 'default',
+	// Then proceed to concatenate our document type
+	// to the ones provided by any plugins that are installed
+	types: schemaTypes.concat([
+		/* Your types here! */
+		post,
+		category,
+		author,
+		authorReference,
+		mainImage,
+		bioPortableText,
+		excerptPortableText,
+		bodyPortableText,
+	]),
+});
