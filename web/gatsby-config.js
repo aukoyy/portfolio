cdfe99cc1d0ca0869b9ddb299330f490a58396d5
aukoyy <22494963+aukoyy@users.noreply.github.com>
2021-04-12 13:41:41 +0200

    add token

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         dataset: 'production',
         // a token with read permissions is required
         // if you have a private dataset
-        token: process.env.MY_SANITY_TOKEN,
+        token: process.env.SANITY_READ_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
